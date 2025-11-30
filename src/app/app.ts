5a2496e2697f8a6def18913a98097b586bb57c05
RamD446 <ramd446@example.com>
2025-11-30 06:52:21 +0530

    Your commit message

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,37 @@
 import { Component, signal } from '@angular/core';
-
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Dashboard } from './components/dashboard/dashboard';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header,Footer,Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('rdmwebtech');
+  ngOnInit(): void {
+    const body = document.body;
+    const themeBtn = document.getElementById("themeBtn") as HTMLButtonElement;
+    const themeIcon = document.getElementById("themeIcon") as HTMLElement;
+
+    // Load saved theme
+    if (localStorage.getItem("theme") === "dark") {
+      body.classList.add("dark-mode");
+      themeIcon.classList.replace("bi-moon-fill", "bi-sun-fill");
+    }
+
+    // Toggle theme on click
+    themeBtn.addEventListener("click", () => {
+      body.classList.toggle("dark-mode");
+
+      if (body.classList.contains("dark-mode")) {
+        themeIcon.classList.replace("bi-moon-fill", "bi-sun-fill");
+        localStorage.setItem("theme", "dark");
+      } else {
+        themeIcon.classList.replace("bi-sun-fill", "bi-moon-fill");
+        localStorage.setItem("theme", "light");
+      }
+    });
+  }
 }
